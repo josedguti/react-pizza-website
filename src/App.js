7644bd04e994460e080df3eea37e2d152a122cf0
Jose Gutierrez <josegutierrez@Joses-Mac-mini.fios-router.home>
2021-06-10 19:02:26 -0400

    finish pizza website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar'
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { GlobalStyle } from './globalStyles';
+import { productData, productDataTwo } from './components/Products/data'
+import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <GlobalStyle />
+      <Hero />
+      <Products heading='Choose your favorite' data={productData} />
+      <Feature />
+      <Products heading='Sweet Treats for You' data={productDataTwo} />
+      <Footer />
     </Router>
   );
 }
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 import { NavLink as Link } from 'react-router-dom'
+import { FaPizzaSlice } from 'react-icons/fa'
 
 export const Nav = styled.nav`
-    background: red;
+    background: transparent;
     height: 80px;
     display: flex;
     justify-content: center;
@@ -28,5 +29,17 @@ export const NavIcon = styled.div`
     display: block;
     position: absolute;
     top: 0;
-    
+    right: 0;
+    cursor: pointer;
+    color: #fff;
+
+    p {
+        transform: translate(-175%, 100%);
+        font-weight: bold;
+    }
+`
+
+export const Bars = styled(FaPizzaSlice)`
+    font-size: 2rem;
+    transform: translate(-50%, -15%);
 `
